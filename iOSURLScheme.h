efb91586e93d4bdd2238810d72ca1a9d391cbabe
马雨辰 <511392597@qq.com>
2017-02-04 16:47:49 +0800

    整理

---
+++ iOSURLScheme.h
@@ -0,0 +1,63 @@
+电池电量 Prefs:root=BATTERY_USAGE
+通用设置 Prefs:root=General
+存储空间 Prefs:root=General&path=STORAGE_ICLOUD_USAGE/DEVICE_STORAGE
+蜂窝数据 Prefs:root=MOBILE_DATA_SETTINGS_ID
+Wi-Fi 设置 Prefs:root=WIFI
+蓝牙设置 Prefs:root=Bluetooth
+定位设置 Prefs:root=Privacy&path=LOCATION
+辅助功能 Prefs:root=General&path=ACCESSIBILITY
+关于手机 Prefs:root=General&path=About
+键盘设置 Prefs:root=General&path=Keyboard
+显示设置 Prefs:root=DISPLAY
+声音设置 Prefs:root=Sounds
+App Store 设置 Prefs:root=STORE
+墙纸设置 Prefs:root=Wallpaper
+打开电话 Mobilephone://
+世界时钟 Clock-worldclock://
+闹钟 Clock-alarm://
+秒表 Clock-stopwatch://
+倒计时 Clock-timer://
+打开相册 Photos://
+
+//以上收集来自钟颖大神的GitHub  https://github.com/cyanzhong/app-tutorials/blob/master/schemes.md
+
+prefs:root=General&path=About //关于本机
+prefs:root=General&path=ACCESSIBILITY //辅助功能
+prefs:root=AIRPLANE_MODE //飞行模式
+prefs:root=General&path=AUTOLOCK //自动锁定
+prefs:root=General&path=USAGE/CELLULAR_USAGE
+prefs:root=Brightness
+prefs:root=Bluetooth //蓝牙
+prefs:root=General&path=DATE_AND_TIME
+prefs:root=FACETIME
+prefs:root=General
+prefs:root=General&path=Keyboard
+prefs:root=CASTLE
+prefs:root=CASTLE&path=STORAGE_AND_BACKUP
+prefs:root=General&path=INTERNATIONAL
+prefs:root=LOCATION_SERVICES
+prefs:root=ACCOUNT_SETTINGS
+prefs:root=MUSIC
+prefs:root=MUSIC&path=EQ
+prefs:root=MUSIC&path=VolumeLimit
+prefs:root=General&path=Network
+prefs:root=NIKE_PLUS_IPOD
+prefs:root=NOTES
+prefs:root=NOTIFICATIONS_ID
+prefs:root=Phone
+prefs:root=Photos
+prefs:root=General&path=ManagedConfigurationList
+prefs:root=General&path=Reset
+prefs:root=Sounds&path=Ringtone
+prefs:root=Safari
+prefs:root=General&path=Assistant
+prefs:root=Sounds
+prefs:root=General&path=SOFTWARE_UPDATE_LINK
+prefs:root=STORE
+prefs:root=TWITTER
+prefs:root=FACEBOOK
+prefs:root=General&path=USAGE prefs:root=VIDEO
+prefs:root=General&path=Network/VPN
+prefs:root=Wallpaper
+prefs:root=WIFI
+prefs:root=INTERNET_TETHERING
